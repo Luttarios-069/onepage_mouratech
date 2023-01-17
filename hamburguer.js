4aa9a76b4d7a9b17e1ad2d6ff819769e54a3d81f
Luttarios-069 <117871011+Luttarios-069@users.noreply.github.com>
2023-01-17 10:43:33 -0300

    nav-bar responsive

--- hamburguer.js
+++ hamburguer.js
@@ -1,15 +1,29 @@
+let fechar = window.document.querySelector('i#close');
+let imgHamburguer = window.document.querySelector('i#burguer')
+let nav = window.document.querySelector('nav');
+let img = window.document.querySelector('img#logo_site');
+let header = window.document.querySelector('header');
+
 function openMenu() {
-    let nav = window.document.querySelector('nav');
-    let img = window.document.querySelector('img#logo_site');
-    let imgHamburguer = window.document.querySelector('i#burguer')
     let ul = window.document.querySelector('ul#ul_nav');
-    let header = window.document.querySelector('header');
     header.style.flexDirection = 'column';
     nav.style.display = 'flex';
-    imgHamburguer.style.justifyContent = 'flex-end';
+    imgHamburguer.style.display = 'none';
     ul.style.marginRight = '0px'
     img.style.display = 'none';
     ul.style.flexDirection = 'column'
     ul.style.backgroundColor = 'black';
-    ul.style.alignItems = 'center'
+    ul.style.alignItems = 'center';
+    fechar.style.display = 'flex'
+    fechar.style.justifyContent = 'flex-end'
+}
+
+function closeMenu() {
+    imgHamburguer.style.display = 'flex';
+    imgHamburguer.style.justifyContent = 'flex-end';
+    fechar.style.display = 'none';
+    nav.style.display = 'none';
+    img.style.display = 'flex';
+    header.style.display = 'flex';
+    header.style.flexDirection = 'row';
 }
